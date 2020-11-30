6b58167a73b73a9287efeda91179a9dd33e4e2bf
blyo <blyo@benjis-MacBook-Pro.local>
2020-11-30 14:27:03 -0600

    20201130 -01

---
+++ file/newrow-input.py
@@ -0,0 +1,3 @@
+fave_num = input('What number?')
+print ('Your favorite number is ' + fave_num)
+
---
+++ file/newrow.py
@@ -0,0 +1,12 @@
+print (2*3*5)
+
+print ("abc" + "bde")
+
+print (5%2)
+
+print ('abc' * 3 + 'd')
+
+subject = 'programming'
+print (subject + ' is my favorite subject')
+
+print (str(1) + 'asdadad')
